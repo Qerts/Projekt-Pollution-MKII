75716058dbaa8086138c42c3d71e27768074e363
Qerts <michal.balicky@gmail.com>
2014-09-06 11:25:20 +0200

    Pollution v0.0.0.2
    Základní layout.
    
    Goal:
    - vytvořit třídu Tile
    - nahodit hlavní status tak, aby byl neustále na stejném místě i po
    otočení

---
+++ Pollution/Pollution/Pollution.Shared/Tile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml;
+
+namespace Pollution 
+{
+    public class Tile 
+    {
+        public Tile()
+        {
+            
+        }
+
+        public enum TileType 
+        {
+            TestTile,
+            BlankTile,
+            ImageTile,
+            StringTile,
+            StatusTile_SO2,
+            StatusTile_O3,
+            StatusTile_CO,
+            StatusTile_PM10,
+            StatusTile_NO2,
+            MainStatusTile,
+            ButtonTile_MenuPanel,
+            ButtonTile_DataPanel,
+            ButtonTile_MapPanel,
+        }
+    }
+}
